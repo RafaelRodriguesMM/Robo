60f6c2f13a2c9489112cdc36b8f59d9ad85a887e
Rafaelmoscosoo <rafatablete@gmail.com>
2025-06-25 21:37:01 -0300

    código_jogo_de_robo
    Adicionei o código do jogo

---
+++ Jogo_de_robo/jogo_de_robo.py
@@ -0,0 +1,89 @@
+from numpy import full
+from random import randint
+
+#gera a matriz 10 x 10 com as posicões vazias " "
+matriz = full((10, 10), " ")
+
+#O robo comeca da primeira posição para o jogador poder ir movendo a partir dai
+matriz[0][0] = "R"
+
+
+#preenche a matriz como 5 pacotes(#) em posições aleatorias
+i = 0
+while i < 5:
+    Linha_aleatoria = randint(0,9)
+    coluna_aleatoria = randint(0,9)
+    # imperde que um pacote seja colocado na posição ocupada pelo robo ou por outro pacote
+    if matriz[Linha_aleatoria][coluna_aleatoria] in "R#":
+        ""
+    else:
+        matriz[Linha_aleatoria][coluna_aleatoria] = "#" 
+        i = i + 1
+
+print("AJUDE O ROBO A COLETAR OS PACOTES: ")
+
+
+while True:
+    print(matriz)
+
+    #Acha a posição atual do robo
+    for linha in range(0,10):
+        for coluna in range(0,10):
+            if matriz[linha][coluna] == "R":
+                Linha_atual = linha
+                coluna_atual = coluna
+                break
+        if matriz[linha][coluna] == "R":
+                break
+    
+    #Verifica se ha pacotes restantes(o jogo continua, ou não há pacotes restantes(o jogador venceu))
+    pacotes = 0
+    for linha in range(0,10):
+        for coluna in range(0,10):
+            if matriz[linha][coluna] == '#':
+                pacotes += 1
+                
+    if pacotes > 0:
+        ''
+    else:
+        print("PARABENS, VOCÊ COLETOU TODOS OS PACOTES")
+        print("FIM DE JOGO")
+        break
+        
+    movimento = str(input("PARA ONDE DESEJA IR:\n[A]esquerda\n[D]direirta\n[W]frente\n[S]baixo\n[Q]sair do jogo\n: "))
+
+    if movimento.lower() == "a":
+        #Ir par a esquerda é mover para a coluna atual - 1, se o jogador tentar ir para a esquerda na coluna de indice 0 o robo não teria casa para ir.
+        if (coluna_atual - 1 < 0):
+            print("O robo não consegui ir mais para a esquerda, tente outro movimento")
+        else:
+            #A Posição atual do robo vai ficar vazia novamente e a posição a esquerda, neste movimento, vai ficar o robo
+            matriz[Linha_atual][coluna_atual] = ' '
+            matriz[Linha_atual][coluna_atual - 1] = 'R'
+
+    elif movimento.lower() == "d":  
+        if (coluna_atual + 1 > 9):
+            print("O robo não consegui ir mais para a direita, tente outro movimento")
+        else:
+            matriz[Linha_atual][coluna_atual] = ' '
+            matriz[Linha_atual][coluna_atual + 1] = 'R'
+
+    elif movimento.lower() == "w": 
+        if (Linha_atual - 1 < 0):
+            print("O robo não consegui ir mais para cima, tente outro movimento")
+        else:
+            matriz[Linha_atual][coluna_atual] = ' '
+            matriz[Linha_atual - 1][coluna_atual] = 'R'
+
+    elif movimento.lower() == "s":  
+        if (Linha_atual + 1 > 9):
+            print("O robo não consegui ir mais para baixo, tente outro movimento")
+        else:
+            matriz[Linha_atual][coluna_atual] = ' '
+            matriz[Linha_atual + 1][coluna_atual] = 'R'
+
+    elif movimento.lower() == "q":
+        print("obrigado por jogar o jogo!")
+        break
+    else:
+        print("digite um movimento valido")
